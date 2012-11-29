72fef33a777940adf9702bcc2a4a2bb6575174a0
Joan Zapata <jzapata@excilys.com>
2012-11-29 16:48:33 +0100

    Improved content background, spacing, hr

--- js/jquery/navbar.js
+++ js/jquery/navbar.js
@@ -8,7 +8,6 @@ $(function() {
  	var navbar = $(".fixed-navbar");
  	var body = $("body");
 
- 	console.log(navbarLimit);
 	var adjustNavbar = function(){
 		if (body.scrollTop() > navbarLimit){
 			// Set visible
