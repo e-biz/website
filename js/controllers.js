12dece87c5c1db83eadbdec2c17f5f1338b69b18
Joan Zapata <jzapata@excilys.com>
2013-06-18 17:24:41 +0200

    Fixed bug with Safari and ng-click containing ng-click

--- js/controllers.js
+++ js/controllers.js
@@ -3,94 +3,100 @@
 /* Controllers */
 
 
-function Links($scope){
+function Links($scope) {
 
-	$scope.redirect = function(url) {
-		window.open (url);
-	}
+    $scope.redirect = function (url, $event) {
+        if ($event) {
+            if ($event.stopPropagation) $event.stopPropagation();
+            if ($event.preventDefault) $event.preventDefault();
+            $event.cancelBubble = true;
+            $event.returnValue = false;
+        }
+        window.open(url);
+    }
 
 }
 
-function Internships($scope){
-	$scope.download = function(url){
-		window.open (url);
-	}
+function Internships($scope) {
+    $scope.download = function (url) {
+        window.open(url);
+    }
 
-	$scope.internships = [
+    $scope.internships = [
 
-		{title : "Serious game Android et iPad",
-		pdf : "http://www.ebusinessinformation.fr/images/stories/ebi/Stages2013/ebi.stages2013.androidios.pdf",
-		description : [
-			 "Outils et normes Java/JEE",
-             "Serveurs d’applications JBoss/Tomcat",
-             "Web services, JSON",
-             "Android, IOS (IPhone, Ipod, IPad), AndroidAnnotations",
-             "La méthode agile SCRUM"
-		]},
+        {title: "Serious game Android et iPad",
+            pdf: "http://www.ebusinessinformation.fr/images/stories/ebi/Stages2013/ebi.stages2013.androidios.pdf",
+            description: [
+                "Outils et normes Java/JEE",
+                "Serveurs d’applications JBoss/Tomcat",
+                "Web services, JSON",
+                "Android, IOS (IPhone, Ipod, IPad), AndroidAnnotations",
+                "La méthode agile SCRUM"
+            ]},
 
-		{title : "Capico Mobile",
-		pdf : "http://www.ebusinessinformation.fr/images/stories/ebi/Stages2013/ebi.stages2013.capicomobile.pdf",
-		description : [
-			 "Outils et normes Java/JEE",
-             "Serveurs d’applications JBoss / Tomcat", 
-			 "Web services, JSON",
-             "Plateforme Android (google), IOS (IPhone, Ipod, IPad), AndroidAnnotations",
-             "La méthode agile SCRUM pour le suivi du projet"
-		]},
+        {title: "Capico Mobile",
+            pdf: "http://www.ebusinessinformation.fr/images/stories/ebi/Stages2013/ebi.stages2013.capicomobile.pdf",
+            description: [
+                "Outils et normes Java/JEE",
+                "Serveurs d’applications JBoss / Tomcat",
+                "Web services, JSON",
+                "Plateforme Android (google), IOS (IPhone, Ipod, IPad), AndroidAnnotations",
+                "La méthode agile SCRUM pour le suivi du projet"
+            ]},
 
-		{title : "Gatling Stress Tool",
-		pdf : "http://www.ebusinessinformation.fr/images/stories/ebi/Stages2013/ebi.stages2013.gatlingscalajee.pdf",
-		description : [
-			"Scala",
-			"Akka",
-			"Eclipse",
-			"Diverses librairies Java"
-		]},
+        {title: "Gatling Stress Tool",
+            pdf: "http://www.ebusinessinformation.fr/images/stories/ebi/Stages2013/ebi.stages2013.gatlingscalajee.pdf",
+            description: [
+                "Scala",
+                "Akka",
+                "Eclipse",
+                "Diverses librairies Java"
+            ]},
 
-		{title : "Intelligence économique",
-		pdf : "http://www.ebusinessinformation.fr/images/stories/ebi/Stages2013/ebi.stages2013.intellecoreseauxsociaux.pdf",
-		description : [
-			"Outils et normes Java/JEE",
-			"Serveurs d’applications JBoss / Tomcat",
-			"Frameworks Spring, Hibernate, JUnit, Webservice",
-			"Mysql",
-			"Map reduce comme Hadoop",
-			"La méthode agile SCRUM pour le suivi du projet"
-		]},
+        {title: "Intelligence économique",
+            pdf: "http://www.ebusinessinformation.fr/images/stories/ebi/Stages2013/ebi.stages2013.intellecoreseauxsociaux.pdf",
+            description: [
+                "Outils et normes Java/JEE",
+                "Serveurs d’applications JBoss / Tomcat",
+                "Frameworks Spring, Hibernate, JUnit, Webservice",
+                "Mysql",
+                "Map reduce comme Hadoop",
+                "La méthode agile SCRUM pour le suivi du projet"
+            ]},
 
-		{title : "Cloud computing",
-		pdf : "http://www.ebusinessinformation.fr/images/stories/ebi/Stages2013/ebi.stages2013.capicocloud.pdf",
-		description : [
-			"Serveurs d’applications JBoss / Tomcat",
-			"Amazon, AppEngine, Cloud Foundry",
-			"Flex, Flash",
-  			"Spring, Hibernate, Alfresco",
-  			"La méthode agile SCRUM pour le suivi du projet"
-		]},
+        {title: "Cloud computing",
+            pdf: "http://www.ebusinessinformation.fr/images/stories/ebi/Stages2013/ebi.stages2013.capicocloud.pdf",
+            description: [
+                "Serveurs d’applications JBoss / Tomcat",
+                "Amazon, AppEngine, Cloud Foundry",
+                "Flex, Flash",
+                "Spring, Hibernate, Alfresco",
+                "La méthode agile SCRUM pour le suivi du projet"
+            ]},
 
-		{title : "Capico JavaEE",
-		pdf : "http://www.ebusinessinformation.fr/images/stories/ebi/Stages2013/ebi.stages2013.capicojee.pdf",
-		description : [
-			"Outils et normes Java/JEE",
-			"Serveurs d’applications JBoss / Tomcat",
-			"Web services",
-			"Flex",
-			"Format Open Document",
-			"La méthode agile SCRUM pour le suivi du projet"
-		]},
+        {title: "Capico JavaEE",
+            pdf: "http://www.ebusinessinformation.fr/images/stories/ebi/Stages2013/ebi.stages2013.capicojee.pdf",
+            description: [
+                "Outils et normes Java/JEE",
+                "Serveurs d’applications JBoss / Tomcat",
+                "Web services",
+                "Flex",
+                "Format Open Document",
+                "La méthode agile SCRUM pour le suivi du projet"
+            ]},
 
-		{title : "Interface papier/crayon",
-		pdf : "http://www.ebusinessinformation.fr/images/stories/ebi/Stages2013/ebi.stages2013.capicopapiercrayon.pdf",
-		description : [
-			"Outils et normes Java/JEE",
-			"Serveurs d’applications JBoss / Tomcat",
-			"Web services",
-			"Flex",
-			"Format Open Document",
-			"La méthode agile SCRUM pour le suivi du projet",
-			"Connaissance en traitement d’images est un plus"
-		]},
-	];
+        {title: "Interface papier/crayon",
+            pdf: "http://www.ebusinessinformation.fr/images/stories/ebi/Stages2013/ebi.stages2013.capicopapiercrayon.pdf",
+            description: [
+                "Outils et normes Java/JEE",
+                "Serveurs d’applications JBoss / Tomcat",
+                "Web services",
+                "Flex",
+                "Format Open Document",
+                "La méthode agile SCRUM pour le suivi du projet",
+                "Connaissance en traitement d’images est un plus"
+            ]},
+    ];
 
 }
 
