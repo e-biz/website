d7934c4b88ae0426e853e91496eeddc8e9db7d7d
anthonysalembier <asalembier@excilys.com>
2015-11-06 17:41:05 +0100

    [Update] Add Menu link + Update internship offers

--- js/controllers.js
+++ js/controllers.js
@@ -20,12 +20,12 @@ function Links($scope) {
 function Internships($scope) {
     $scope.download = function (url) {
         window.open(url);
-    }
+    };
 
     $scope.internships = [
 
         {title: "Serious game Android et iPad",
-            pdf: "http://www.ebusinessinformation.fr/img/stages2015/REC.StageG3.Serious_Game_Tablette_Android_IPad.2015.T1.1.pdf",
+            pdf: "http://www.ebusinessinformation.fr/img/stages2015/REC.StageG3.Serious_Game_Tablette_Android_IPad.2016.T1.1.pdf",
             description: [
                 "Outils et normes Java/JEE",
                 "Serveurs d’applications JBoss/Tomcat",
@@ -35,17 +35,17 @@ function Internships($scope) {
             ]},
 
         {title: "Capico Mobile",
-            pdf: "http://www.ebusinessinformation.fr/img/stages2015/REC.StageG3.CAPICO_Android_IPad.2015.T1.1.pdf",
+            pdf: "http://www.ebusinessinformation.fr/img/stages2015/REC.StageG3.CAPICO_Android_IPad.2016.T1.1.pdf",
             description: [
                 "Outils et normes Java/JEE",
-                "Serveurs d’applications JBoss / Tomcat",
+                "Serveurs d’applications Tomcat",
                 "Web services, JSON",
                 "Plateforme Android (google), IOS (IPhone, Ipod, IPad), AndroidAnnotations",
                 "La méthode agile SCRUM pour le suivi du projet"
             ]},
 
         {title: "Gatling Stress Tool",
-            pdf: "http://www.ebusinessinformation.fr/img/stages2015/REC.StageG3.Gatling.2015.T1.1.pdf",
+            pdf: "http://www.ebusinessinformation.fr/img/stages2015/REC.StageG3.Gatling.2016.T1.1.pdf",
             description: [
                 "Scala",
                 "Akka",
@@ -54,38 +54,17 @@ function Internships($scope) {
             ]},
 
         {title: "Capico HTML5, CSS3, AngularJS",
-            pdf: "http://www.ebusinessinformation.fr/img/stages2015/REC.StageG3.CAPICO_HTML5_CSS3.2015.T1.2.pdf",
+            pdf: "http://www.ebusinessinformation.fr/img/stages2015/REC.StageG3.CAPICO_HTML5_CSS3.2016.T1.1.pdf",
             description: [
                 "Outils et normes Java/JEE",
-                "Serveurs d’applications JBoss / Tomcat",
+                "Serveurs d’applications Tomcat",
                 "Web services",
                 "Plateforme Android, Ipad, Surface",
                 "La méthode agile SCRUM pour le suivi du projet"
             ]},
 
-        {title: "Cloud computing",
-            pdf: "http://www.ebusinessinformation.fr/img/stages2015/REC.StageG3.JEE.Capico_CloudComputing.2015.T1.1.pdf",
-            description: [
-                "Serveurs d’applications JBoss / Tomcat",
-                "Amazon, AppEngine, Cloud Foundry",
-                "Flex, Flash",
-                "Spring, Hibernate, Alfresco",
-                "La méthode agile SCRUM pour le suivi du projet"
-            ]},
-
-        {title: "Capico JavaEE",
-            pdf: "http://www.ebusinessinformation.fr/img/stages2015/REC.StageG3.JEE.Capico.2015.T1.1.pdf",
-            description: [
-                "Outils et normes Java/JEE",
-                "Serveurs d’applications JBoss / Tomcat",
-                "Web services",
-                "Flex",
-                "Format Open Document",
-                "La méthode agile SCRUM pour le suivi du projet"
-            ]},
-
         {title: "Design My App",
-            pdf: "http://www.ebusinessinformation.fr/img/stages2015/REC.StageG3.DESIGNMYAPP_JAVA_HTML5_CSS3_ANGULARJS_ANDROID_CORDOVA.2015.T1.2.pdf",
+            pdf: "http://www.ebusinessinformation.fr/img/stages2015/REC.StageG3.DESIGNMYAPP_JAVA_HTML5_CSS3_ANGULARJS_ANDROID_CORDOVA.2016.T1.1.pdf",
             description: [
                 "Outils et normes Java/JEE, Spring, Maven",
 		"MongoDB",
